a4f00ede709561a37e7a4b41fb1b70724ad6ac52
aglide100 <aglide100@gmail.com>
2021-06-23 14:32:07 +0900

    deploy yaml추가

---
+++ buildAndRun.sh
@@ -0,0 +1,23 @@
+export DockerService=~/scroll-react
+
+#!/bin/bash
+
+echo "====> Service Close Docker-compose"
+cd $DockerService
+docker-compose down --remove-orphans
+
+echo "====> Remove docker network"
+cd $DockerService
+docker network prune -f
+
+echo "====> Remove unused Conataner"
+cd $DockerService
+docker image prune -a -f
+
+echo "====> Build Docker-Compose"
+cd $DockerService
+docker-compose build
+
+echo "====> Starting Docker-Compse daemon"
+cd $DockerService
+docker-compose up -d
