9a1ba70e6cdeec499086762ed7e1d76d2aa8553c
HeeJun Moon <aglide100@gmail.com>
2021-06-29 19:11:00 +0900

    Delete .history directory

--- .history/ui/pages/index_20210617145104.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useScrollHooks } from "../src/hooks/useScroollHooks";
-
-export default function Home() {
-  const testHook = useScrollHooks();
-
-  return <div></div>;
-}
