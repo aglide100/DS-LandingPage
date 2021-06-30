fb7bf32bfb68b39bca9d5d62f417de19424b8e1e
aglide100 <aglide100@gmail.com>
2021-06-30 09:46:54 +0900

    Dockerfile 수정

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -21,8 +21,8 @@ export function Header() {
         className={classNames(
           "group w-screen fixed left-0 top-0 box-border transition duration-500 z-30",
           {
-            "h-10 bg-gray-300 bg-opacity-50 transform -translate-y-4 hover:translate-y-0 hover:bg-gray-500 hover:bg-opacity-100":
-              result.scrollY > 100,
+            "h-10 bg-gray-300 bg-opacity-50 transform -translate-y-4 hover:-translate-y-2 hover:bg-gray-500 hover:bg-opacity-100":
+              result.scrollY > 100 || result.scrollDirection == "up",
             "h-20 bg-white ":
               result.scrollY < 100 || result.scrollDirection == "down"
                 ? true
@@ -34,7 +34,8 @@ export function Header() {
           className={classNames(
             "cursor-pointer flex justify-around items-center mt-3 mb-3 transition duration-500",
             {
-              "text-gray-300 group-hover:text-black": result.scrollY > 100,
+              "text-gray-300 group-hover:text-black":
+                result.scrollY > 100 || result.scrollDirection == "up",
               "text-black":
                 result.scrollY < 100 || result.scrollDirection == "down"
                   ? true
@@ -42,18 +43,25 @@ export function Header() {
             }
           )}
         >
-          <span>Hello!</span>
-          <span>Foo</span>
-          <span>Bar</span>
-          <span
+          <span>Logo</span>
+
+          <a
+            className={classNames("menu-trigger type12", {
+              "active-12": isIconClick,
+              "": !isIconClick,
+            })}
+            // href="#"
             onClick={() => {
               setIconClick(!isIconClick);
             }}
           >
-            햄버그 아이콘
-          </span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
         </div>
       </nav>
+
       <div
         className={classNames(
           "fixed bg-green-100 mt-8 w-screen h-screen z-20",
