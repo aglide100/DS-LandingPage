921b88f42751f023bd23a6ec85df12598423de3d
aglide100 <aglide100@gmail.com>
2021-07-13 19:55:51 +0900

    인트로 영상 정지안되는 현상 수정, docker-compose 버전 변경

--- ui/src/components/layout/section/InterViewSectionWrapper/InterviewSectionWrapper.tsx
+++ ui/src/components/layout/section/InterViewSectionWrapper/InterviewSectionWrapper.tsx
@@ -9,13 +9,32 @@ const InterViewSectionWrapper: React.FC<InterViewSectionWrapperProps> = (
 ) => {
   return (
     <div
-      className="w-full min-h-screen pl-48 pr-48"
+      className="w-full min-h-screen pl-48 pr-48 relative"
       style={{
         background: `repeating-linear-gradient(135deg, #404040, #404040 2px, #1f1f1f 1px, #1f1f1f 8px)`,
       }}
     >
-      compute converge create
-      {props.children}
+      <div className="absolute top-0 right-0  flex flex-col">
+        <div
+          style={{ color: "#545454" }}
+          className="text-bg cursor-default mix-blend-overlay "
+        >
+          Compute
+        </div>
+        <div
+          style={{ color: "#545454" }}
+          className="text-bg cursor-default mix-blend-overlay "
+        >
+          Converge
+        </div>
+        <div
+          style={{ color: "#545454" }}
+          className="text-bg cursor-default mix-blend-overlay "
+        >
+          Create
+        </div>
+      </div>
+      <div>{props.children}</div>
     </div>
   );
 };
