559dc65a51de8958dcaa9184369ab3217d9d30f7
JaeYeolPark <20161547@g.dongseo.ac.kr>
2021-08-05 13:09:41 +0900

    TrophySection, tailwind.config.js

--- ui/src/components/layout/TrophySection/TrophySection.tsx
+++ ui/src/components/layout/TrophySection/TrophySection.tsx
@@ -1,7 +1,67 @@
 import React from "react";
 
 const TrophySection: React.FC<{}> = () => {
-  return <></>;
+  return (
+    <div>      
+      <div className="pt-32 mx-20 flex flex-col justify-center">
+        <div className="h-20 mt-10 mx-36 pt-3 bg-black rounded-xl text-6xl text-white text-center">
+          주요성과
+        </div>
+        <div className="flex flex-wrap pt-16 justify-center">
+          <div className="w-64 h-80 mx-2 content-center text-center bg-white rounded-xl shadow-self">
+            1
+          </div >
+          <div className="w-64 h-80 mx-2 content-center text-center bg-white rounded-xl shadow-self">
+            1
+          </div>
+          <div className="w-64 h-80 mx-2 content-center text-center bg-white rounded-xl shadow-self">
+            1
+          </div>
+          <div className="w-64 h-80 mx-2 content-center text-center bg-white rounded-xl shadow-self">
+            1
+          </div>
+          <div className="w-64 h-80 mx-2 content-center text-center bg-white rounded-xl shadow-self">
+            1
+          </div>
+        </div>
+      </div>
+      
+      <div className="pt-32 mx-20 flex flex-col justify-center">
+        <div className="mt-10 flex flex-wrap pt-16">
+
+          <div className="w-1/4 h-80 content-center text-center bg-red-600">            
+          </div >
+          <div className="w-1/4 h-80 content-center text-center pt-36 text-5xl">
+            상상스퀘어
+          </div>
+
+          <div className="w-1/4 h-80 content-center text-center bg-red-600">
+          </div>
+          <div className="w-1/4 h-80 content-center text-center pt-36 text-5xl">
+            우수한 기자재
+          </div>
+
+          <div className="w-1/4 h-80 content-center text-center pt-36">
+            <div className="">학생들의 창의교육을 완성</div>
+            <div className="text-5xl">Fab Lab</div>
+          </div>
+          <div className="w-1/4 h-80 content-center text-center bg-red-600">
+            1
+          </div>
+
+          <div className="w-1/4 h-80 content-center text-center pt-36 text-5xl">
+            AI+X 융합연구소
+          </div>
+          <div className="w-1/4 h-80 content-center text-center bg-red-600">
+          </div>
+        </div>
+      </div>
+      <div className="mb-96" />
+      
+      {/* <div className="mb-96"></div>
+      <CardContainer2 cardViewList={cardViewContainerProps} /> */}
+    </div>
+  );
 };
 
 export default TrophySection;
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -52,6 +52,25 @@ module.exports = {
       fontSize: {
         bg: "12vw",
       },
+
+      boxShadow: {
+        sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+        DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        'self': '0 30px 45px -12px rgba(0, 0, 0, 0.75)',
+        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        none: 'none',
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#3490dc',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+        'custom': '#212121',
+       })
     },
   },
   variants: {
@@ -59,6 +78,7 @@ module.exports = {
       maxWidth: ["active", "responsive", "hover", "focus", "group-hover"],
       width: ["active", "responsive", "hover", "focus", "group-hover"],
       textColor: ["active", "responsive", "hover", "focus", "group-hover"],
+      boxShadow: ['active'],
     },
   },
   important: true,
